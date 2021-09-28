f7dff01ecee6622083327dc28427f73a9e6b6810
nalonso404 <noemialonso21@gmail.com>
2021-09-28 09:38:28 +0200

    basic estructure

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const TEXT_FONT = "65px Arial";
 const TEXT_COLOR = "#ff0044";
 const TEXT_ALIGNMENT = "center";
 
+
 const game = new Phaser.Game(
     GAME_WIDTH,
     GAME_HEIGHT,
@@ -21,16 +22,48 @@ function preload() {
 }
 
 function create() {
-    let text = game.add.text(
+   //rectangulo resultado
+    let rectangle = game.add.graphics();
+    rectangle.beginFill(0xffffff);
+    rectangle.drawRect(250, 150, 300,100);
+    rectangle.endFill();
+
+    let result = game.add.text(
         game.world.centerX,
         game.world.centerY,
-        "Hola :D",
+        "resultado",
         {
         font: TEXT_FONT,
         fill: TEXT_COLOR,
         align: TEXT_ALIGNMENT
     });
-    text.anchor.setTo(0.5, 0.5);
+    result.anchor.setTo(0.5, 2);
+
+    let sumSquare = game.add.graphics();
+    sumSquare.beginFill(0xffffff);
+    sumSquare.drawRect(250, 300, 100,100);
+    sumSquare.endFill();
+
+    // let sumOperator= game.add.text(
+    //     250,
+    //     300,
+    //     "+",
+    //     {
+    //     font: TEXT_FONT,
+    //     fill: TEXT_COLOR,
+    //     align: TEXT_ALIGNMENT});
+    //     sumOperator.anchor.setTo(-0.75, 0.5);
+
+    let substractSquare = game.add.graphics();
+    substractSquare.beginFill(0xffffff);
+    substractSquare.drawRect(450, 300, 100,100);
+    substractSquare.endFill();
+
+    //buttons & counter
+
+    
+
+
 }
 
 function update() {
