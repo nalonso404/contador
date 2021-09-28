ded31ccdabbe9107ac3d943e8fa0e2231174733e
nalonso404 <noemialonso21@gmail.com>
2021-09-28 11:56:06 +0200

    basic sum and subs logic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const TEXT_COLOR = "#ff0044";
 const TEXT_ALIGNMENT = "center";
 
 
+let counter = 0;
+let result;
+let sumSquare
+let substractSquare
+
+
 const game = new Phaser.Game(
     GAME_WIDTH,
     GAME_HEIGHT,
@@ -28,10 +34,10 @@ function create() {
     rectangle.drawRect(250, 150, 300,100);
     rectangle.endFill();
 
-    let result = game.add.text(
+    result = game.add.text(
         game.world.centerX,
         game.world.centerY,
-        "resultado",
+        counter,
         {
         font: TEXT_FONT,
         fill: TEXT_COLOR,
@@ -39,33 +45,79 @@ function create() {
     });
     result.anchor.setTo(0.5, 2);
 
-    let sumSquare = game.add.graphics();
+    sumSquare = game.add.graphics();
     sumSquare.beginFill(0xffffff);
     sumSquare.drawRect(250, 300, 100,100);
     sumSquare.endFill();
 
-    // let sumOperator= game.add.text(
-    //     250,
-    //     300,
-    //     "+",
-    //     {
-    //     font: TEXT_FONT,
-    //     fill: TEXT_COLOR,
-    //     align: TEXT_ALIGNMENT});
-    //     sumOperator.anchor.setTo(-0.75, 0.5);
+    let sumOperator= game.add.text(
+        250,
+        300,
+        "+",
+        {
+        font: TEXT_FONT,
+        fill: TEXT_COLOR,
+        align: TEXT_ALIGNMENT});
+        sumOperator.anchor.setTo(-0.75, -0.15);
 
-    let substractSquare = game.add.graphics();
+    substractSquare = game.add.graphics();
     substractSquare.beginFill(0xffffff);
     substractSquare.drawRect(450, 300, 100,100);
     substractSquare.endFill();
 
-    //buttons & counter
-
+    let substractOperator= game.add.text(
+        470,
+        270,
+        "-",
+        {
+        font: TEXT_FONT,
+        fill: TEXT_COLOR,
+        align: TEXT_ALIGNMENT});
+        substractOperator.anchor.setTo(-0.75, -0.5);
+        
+        createEvent()
     
-
-
 }
 
+function createEvent () {
+    if (counter === 0) {
+        sumSquare.inputEnabled = true;
+        sumSquare.input.useHandCursor = true;
+        sumSquare.events.onInputDown.add(sumHandler, this);
+
+        substractSquare.inputEnabled = false;
+
+    } else if (counter === 10){
+        sumSquare.inputEnabled = false;
+        
+        substractSquare.inputEnabled = true;
+        substractSquare.input.useHandCursor = true;
+        substractSquare.events.onInputDown.add(substractHandler, this);
+    }
+    else {
+        sumSquare.inputEnabled = true;
+        sumSquare.input.useHandCursor = true;
+        sumSquare.events.onInputDown.add(sumHandler, this);
+
+        substractSquare.inputEnabled = true;
+        substractSquare.input.useHandCursor = true;
+        substractSquare.events.onInputDown.add(substractHandler, this);
+    }
+    
+}
+
+function sumHandler() {
+    counter ++
+    result.text = counter;
+    createEvent()
+
+}
+function substractHandler () {
+    counter --
+    result.text=counter;
+    createEvent()
+
+}
 function update() {
 
 }
